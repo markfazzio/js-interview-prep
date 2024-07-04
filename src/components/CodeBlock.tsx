10d2da5a617535333abc0f812f6ac6ab276fe049
Mark Fazzio <mark@pixelpandas.com>
2024-07-03 22:51:06 -0400

    fixed build

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,16 +1,14 @@
 import { ThemeContext, ThemeContextType } from "@/context/ThemeContext";
-import { ReactNode, useContext } from "react";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import { useContext } from "react";
+import SyntaxHighlighter, {
+  SyntaxHighlighterProps,
+} from "react-syntax-highlighter";
 import {
   a11yLight,
   agate,
 } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
-interface CodeBlockProps {
-  children: ReactNode;
-}
-
-export const CodeBlock = (props: CodeBlockProps) => {
+export const CodeBlock = (props: SyntaxHighlighterProps) => {
   const { children } = props;
   const { theme } = useContext(ThemeContext) as ThemeContextType;
   const highlighterTheme = theme === "light" ? a11yLight : agate;
